27bf7d7b922da686004c2a35cb7f0d65141a3caf
Dawson7218 <109839047+Dawson7218@users.noreply.github.com>
2026-04-27 10:59:41 -0700

    Added spren, changed mod description, cleaned up shardblade code

---
+++ The Rimlight Archive/Source/RimlightArchive/RimlightArchive/Spren.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RimlightArchive
+{
+// IncidentWorker_ThrumboPasses
+
+    public class SprenSpawnerMapComponent : MapComponent
+    {
+        public SprenSpawnerMapComponent(Map map) : base(map) 
+        {
+        }
+
+        public Map SourceMap => (map.Parent as PocketMapParent)?.sourceMap;
+
+    }
+}
